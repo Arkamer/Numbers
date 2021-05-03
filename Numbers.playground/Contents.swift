281c6356aa8a034013db0b4f95d951327acd604c
Alessasi <ailton.silva@somospi.com.br>
2021-05-03 02:39:48 -0300

    Numbers' exercises

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,53 @@
+
+let numberOfPages: Int = 10
+let numberOfChapters = 3
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
+
+10 + 20
+30 - 5
+5 * 6
+(10 + 2) * 5
+30 - (5 - 5)
+11 / 3
+11 % 3
+-11 % 3
+
+var x = 10
+x += 10
+x -= 5
+
+let y: Int8 = 120
+let z = y &+ 10
+
+let a: Int16 = 200
+let b: Int8 = 50
+let c = a + Int16(b)
+
+let d1 = 1.1
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+10 + 11.4
+11.0 / 3.0
+
+if d1 == d2 {
+    print("d1 and d2 are the same!")
+}
+
+print("d1 + 0.1 is \(d1 + 0.1)")
+if d1 + 0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
+
+let ticketCount = 1_000
+let ticketNumbers = 1 ..< ticketCount
+
+let binaryFail = 10100110
+let binaryInt = 0b10100110
+let scientificInt = 1.66e5
+let fractionalFloat: Float = 1.66e-2
+let hexLiteral = 0xff
+let hexSpeak = 0x8BADF00D
+let lightSpeed = 299_792_458 // m/s
